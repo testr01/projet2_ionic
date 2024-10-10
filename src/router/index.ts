d8bf10deee353951d33a513d2d965c19c04b68d3
testr01 <yvenstest@gmail.com>
2024-10-10 06:18:01 -0400

    correction affichage: remplacé process.env par import.meta.env

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,33 @@
-import { createRouter, createWebHistory } from "@ionic/vue-router";
-import { RouteRecordRaw } from "vue-router";
-import LoginPage from "@/views/LoginPage.vue";
-import LocationPage from "@/views/locationPage.vue";
-import SignupPage from "@/views/signupPage.vue";
-import ProfilePage from "@/views/profilePage.vue";
+import { createRouter, createWebHistory } from '@ionic/vue-router'; // Utilisation d'Ionic Vue Router
+import LocationPage from '@/views/locationPage.vue';
+import ProfilePage from '@/views/profilePage.vue';
+import LoginPage from '@/views/LoginPage.vue';
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
-    path: "/",
-    redirect: "/login",
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: LoginPage,
-  },
-  {
-    path: "/signup",
-    name: "Signup",
-    component: SignupPage,
-  },
-  {
-    path: "/localisation",
-    name: "GéoLocalisation",
+    path: '/location',
+    name: 'LocationPage',
     component: LocationPage,
   },
   {
-    path: "/profile",
-    name: "Profile",
+    path: '/profile',
+    name: 'ProfilePage',
     component: ProfilePage,
   },
-
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage,
+  },
+  {
+    path: '/',
+    redirect: '/location', // Redirection par défaut vers LocationPage
+  },
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL), // Utilisation de Vite pour la base URL
   routes,
 });
 
 export default router;
-
-
